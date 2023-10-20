165f409810a63aeba761075698b2be3627e70915
Sebastián Astudillo <sebastianastudillorojas@gmail.com>
2023-10-20 12:25:04 -0300

    teamContext

--- src/context/TeamContext.tsx
+++ src/context/TeamContext.tsx
@@ -2,18 +2,16 @@
 
 import { Children, createContext, useContext, useEffect, useReducer, useState } from "react";
 import { LoginData, LoginResponse, RegisterData, ResetPasswordData, Usuario } from "../interfaces/appInterfaces";
-import { AuthState, authReducer } from "./AuthReducer";
-import API from "../api/nestApi";
-import axios, { AxiosError } from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import APIteam from "../api/nestApiTeam";
+import { CreateTeamDto, Team } from "../interfaces/teamInterfaces";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 type TeamsContextProps = {
-   teams:Usuario[],
-   loadTeams: ()=> Promise<void>
-   addMember: ( userName:string, rol:string) => Promise<void>
-   updateTeam: ( name:string ,teamId:string) => Promise<void>
-   deleteTeam: ( name:string) => Promise<void>
-   loadTeamById: ( id:string) => Promise<Usuario>
+    createTeam: (createTeamDto: CreateTeamDto)=> Promise<Team|undefined>
+    fetchTeams: () => Promise<void>
+    updateTeam: ( name:string ,teamId:string) => Promise<void>
+    deleteTeam: ( name:string) => Promise<void>
+    loadTeamById: ( id:string) => Promise<Usuario>
 }
 
 
@@ -22,13 +20,45 @@ export const TeamContext = createContext({} as TeamsContextProps);
 export const TeamProvider = ({children}: any) => {
 
 
-const  [teams,setTeams] = useState<Usuario[]>([])
-const loadTeams=  async ()=> {
+const  [teams,setTeams] = useState<Team[]>([])
+const createTeam = async (createTeamDto: CreateTeamDto) => {
+    try {
+      const response: Team = await APIteam.post('/api/teams', createTeamDto);
+      return response
+    } catch (error) {
+      console.error(error);
+    }
+};
 
-}
-const  addMember= async ( userName:string, rol:string) => {
+const fetchTeams = async () => {
+    try {
+        const accessToken = await AsyncStorage.getItem("token");
+        const config = {
+            headers: {
+            Authorization: `Bearer ${accessToken}`,
+            },
+        };
+        const response = await APIteam.get('/api/teams', config);
+        setTeams(response.data);
+    } catch (error) {
+        console.error(error);
+    }
+  };
+  const removeTeam = async (uniqueCode: string) => {
+    try {
+      const accessToken = 'tu-token-aqui'; // Reemplaza esto con tu token real
+      const config = {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      };
 
-}
+      const response = await APIteam.delete(`/api/teams/${uniqueCode}`, config);
+      // Puedes manejar la respuesta o simplemente completar la solicitud con éxito
+    } catch (error) {
+      console.error(error);
+    }
+  };
 const  updateTeam= async( name:string ,teamId:string) => {
 
 }
@@ -40,9 +70,8 @@ const  loadTeamById= async ( id:string) => {
 }
     return (
         <TeamContext.Provider value={{
-            teams,
-            loadTeams,
-            addMember,
+            createTeam,
+            fetchTeams,
             updateTeam,
             deleteTeam,
             loadTeamById, 
