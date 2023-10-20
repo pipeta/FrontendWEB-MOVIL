e67bedc116978b4e62c65e74999eb9b0fa887073
felipe <felipe.tapia.guerra@gmail.com>
2023-10-20 10:46:17 -0700

    actualizacion de equipos

--- src/screens/TestingScreen.tsx
+++ src/screens/TestingScreen.tsx
@@ -1,40 +1,52 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { TeamContext } from '../context/TeamContext';
-import { View, FlatList, Text, StyleSheet} from 'react-native';
+import { View, FlatList, Text, StyleSheet } from 'react-native';
 import { Card } from 'react-native-paper';
 import { Button } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
 
 interface TeamData {
+  _id: string;
+  name: string;
+  autor: string;
+  uniqueCode: string;
+  listUser: {
+    userName: string;
+    email: string;
     _id: string;
-    name: string;
-    autor: string;
-    uniqueCode: string;
-    listUser: {
-        userName: string;
-        email: string;
-        _id: string;
-    }[];
+  }[];
 }
 
 export const TestingScreen = () => {
-    const { fetchTeams } = useContext(TeamContext);
-    const { removeTeam } = useContext(TeamContext);
-    const [teams, setTeams] = useState<TeamData[]>([]);
+  const { fetchTeams, removeTeam } = useContext(TeamContext);
+  const [teams, setTeams] = useState<TeamData[]>([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const data: TeamData[] = await fetchTeams();
-                setTeams(data);
-            } catch (error) {
-                console.error(error);
-            }
-        };
+  const fetchData = async () => {
+    try {
+      const data: TeamData[] = await fetchTeams();
+      setTeams(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-        fetchData();
-    }, [fetchTeams]);
+  useFocusEffect(
+    React.useCallback(() => {
+      fetchData();
 
-    return (
+      // Limpia el estado cuando la pantalla se desenfoca
+      return () => {
+        setTeams([]);
+      };
+    }, [fetchTeams])
+  );
+
+  const handleRemoveTeam = async (uniqueCode: string) => {
+    await removeTeam(uniqueCode);
+    fetchData(); // Recarga la lista de equipos después de eliminar un equipo
+  };
+
+  return (
     <View style={styles.container}>
       <Text style={styles.heading}>Lista de Equipos:</Text>
       <FlatList
@@ -59,32 +71,31 @@ export const TestingScreen = () => {
             </Card.Content>
             <Button
               title="Eliminar Equipo"
-              onPress={async () => await removeTeam(team.uniqueCode)} // Reemplaza someTeamId con el ID del equipo que deseas eliminar
+              onPress={() => handleRemoveTeam(team.uniqueCode)}
             />
           </Card>
         )}
       />
     </View>
-    );
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 16,
-    },
-    heading: {
-      fontSize: 24,
-      fontWeight: 'bold',
-      marginBottom: 16,
-    },
-    card: {
-      marginBottom: 16,
-    },
-    teamName: {
-      fontSize: 18,
-      fontWeight: 'bold',
-      marginBottom: 8,
-    },
-  });
-
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  card: {
+    marginBottom: 16,
+  },
+  teamName: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+});
